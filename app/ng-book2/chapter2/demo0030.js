276dacef5dd455b883b8ba8ee33c3dd204fe3474
neil duan <duanyiding@gmail.com>
2015-04-22 15:20:35 +0800

    migrating to 1.3.15

--- app/ng-book2/chapter2/demo0030.js
+++ app/ng-book2/chapter2/demo0030.js
@@ -2,13 +2,37 @@
  * Created by Administrator on 2015/4/12.
  */
 
-function MyController($scope){
-    $scope.clock = {now:new Date()};
-    var updateClock = function(){
+/**
+ * 1.2可以这样声明全局的controller，这在1.3是不允许的。
+ * 1.2的声明：function MyController($scope){}
+ * 1.3应改为如下：
+ */
+angular.module('ngb2.chp2.demo0030', []).controller('MyController', function ($scope) {
+    $scope.clock = {now: new Date()};
+
+    var updateClock = function () {
+
         $scope.clock.now = new Date()
     };
-    setInterval(function(){
+    setInterval(function () {
         $scope.$apply(updateClock);
-    },1000);
+    }, 1000);
     updateClock();
-}
+});
+
+//或者分开写也可以
+/*
+var app = angular.module('ngb2.chp2.demo0030', []);
+app.controller('MyController', function ($scope) {
+    $scope.clock = {now: new Date()};
+
+    var updateClock = function () {
+
+        $scope.clock.now = new Date()
+    };
+    setInterval(function () {
+        $scope.$apply(updateClock);
+    }, 1000);
+    updateClock();
+});
+    */
